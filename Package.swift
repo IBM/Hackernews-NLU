312c9b9aa8da0a03c7a33d6574e5d2204c5e8e7a
Tommy Li <Tommy.chaoping.li@ibm.com>
2017-11-02 14:51:46 -0700

    add new tracker

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
     .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-    .Package(url: "https://github.com/IBM/metrics-collector-client-swift", majorVersion: 5),
+    .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 3),
+    .Package(url: "https://github.com/IBM/metrics-collector-client-swift", majorVersion: 5)
   ]
 )
